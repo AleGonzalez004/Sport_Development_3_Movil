a426093e4190b7a91b21e8f8bb5d3b269690ab34
AleGonzalez002 <138949757+AleGonzalez002@users.noreply.github.com>
2024-06-14 13:41:12 -0600

    Cambios para que funcione

--- src/tabNavigator/TabNavigator.js
+++ src/tabNavigator/TabNavigator.js
@@ -11,41 +11,42 @@ const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
     return (
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            headerShown: false, // Oculta el header
-            tabBarActiveTintColor: '#FFFFFF', // Color de los íconos activos
-            tabBarInactiveTintColor: '#D8D8D8', // Color de los íconos inactivos
-            tabBarStyle: { backgroundColor: '#FFF', height: 60, borderTopWidth: 0 }, // Estilo de la barra de pestañas
-            tabBarIcon: ({ focused, color, size }) => { // Función que define el ícono de la pestaña
-              let iconName;
-              if (route.name === 'Home') {
-                iconName = focused ? 'home' : 'home-outline';
-              } else if (route.name === 'Productos') {
-                iconName = focused ? 'cafe' : 'cafe-outline';
-              } else if (route.name === 'Carrito') {
-                iconName = focused ? 'cart' : 'cart-outline';
-              }
-              return <Ionicons name={iconName} color={color} size={size} />;
-            },
-          })}
-        >
-          <Tab.Screen
-            name="Home"
-            component={Home}
-            options={{ title: 'Inicio' }}
-          />
-          <Tab.Screen
-            name="Productos"
-            component={Productos}
-            options={{ title: 'Productos' }}
-          />
-          <Tab.Screen
-            name="Carrito"
-            component={Carrito}
-            options={{ title: 'Carrito' }}
-          />
-        </Tab.Navigator>
+
+<Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false, // Oculta el header
+        tabBarActiveTintColor: '#FFFFFF', // Color de los íconos activos
+        tabBarInactiveTintColor: '#D8D8D8', // Color de los íconos inactivos
+        tabBarStyle: { backgroundColor: '#FFF', height: 60, borderTopWidth: 0 }, // Estilo de la barra de pestañas
+        tabBarIcon: ({ focused, color, size }) => { // Función que define el ícono de la pestaña
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Productos') {
+            iconName = focused ? 'cafe' : 'cafe-outline';
+          } else if (route.name === 'Carrito') {
+            iconName = focused ? 'cart' : 'cart-outline';
+          }
+          return <Ionicons name={iconName} color={color} size={size} />;
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{ title: 'Inicio' }}
+      />
+      <Tab.Screen
+        name="Productos"
+        component={Productos}
+        options={{ title: 'Productos' }}
+      />
+      <Tab.Screen
+        name="Carrito"
+        component={Carrito}
+        options={{ title: 'Carrito' }}
+      />
+    </Tab.Navigator>
     );
 };
 
