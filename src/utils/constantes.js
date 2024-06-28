524745682b245db5d33e0652cffb73b88e2b623c
20220440 <20220440@ricaldone.edu.sv>
2024-06-28 10:00:55 -0600

    Dependencia

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -7,7 +7,7 @@
 //export const IP = 'http://192.168.1.14';
 
 //colegio
-export const IP = 'http://10.10.1.25';
+export const IP = 'http://10.10.1.103';
 //export const IP = 'http://10.10.0.149';
 
 
