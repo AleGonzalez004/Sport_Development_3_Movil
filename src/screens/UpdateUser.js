7bc182009a568c6c6e74bd9fab1117fec4f3b682
AleGonzalez004 <20220440@ricaldone.edu.sv>
2024-08-02 00:21:30 -0600

    Fecha en update

--- src/screens/UpdateUser.js
+++ src/screens/UpdateUser.js
@@ -1,10 +1,9 @@
-
 import { StyleSheet, Text, Image, View, TouchableOpacity, Alert, ScrollView } from 'react-native';
 import { useState } from 'react';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Constantes from '../utils/constantes'
 import React, { useEffect } from 'react';
-//Import de componentes
+// Import de componentes
 import Input from '../components/Inputs/Input'
 import InputMultiline from '../components/Inputs/InputMultiline'
 import Buttons from '../components/Buttons/Button';
@@ -12,7 +11,6 @@ import MaskedInputTelefono from '../components/Inputs/MaskedInputTelefono';
 import MaskedInputDui from '../components/Inputs/MaskedInputDui';
 import InputEmail from '../components/Inputs/InputEmail';
 import AntDesign from "@expo/vector-icons/AntDesign";
-
 
 export default function getUser({ navigation }) {
   const ip = Constantes.IP;
@@ -22,34 +20,29 @@ export default function getUser({ navigation }) {
   const [mode, setMode] = useState('date');
   const [show, setShow] = useState(false);
   // Estados de los campos de entrada
-  const [nombre, setNombre] = useState('')
-  const [apellido, setApellido] = useState('')
-  const [email, setEmail] = useState('')
-  const [direccion, setDireccion] = useState('')
-  const [dui, setDui] = useState('')
-  const [telefono, setTelefono] = useState('')
-  const [fechaNacimiento, setFechaNacimiento] = useState('')
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
+  const [email, setEmail] = useState('');
+  const [direccion, setDireccion] = useState('');
+  const [dui, setDui] = useState('');
+  const [telefono, setTelefono] = useState('');
+  const [fechaNacimiento, setFechaNacimiento] = useState('');
 
   // Expresiones regulares para validar DUI y teléfono
   const duiRegex = /^\d{8}-\d$/;
   const telefonoRegex = /^\d{4}-\d{4}$/;
 
-  /*
-  Codigo para mostrar el datetimepicker
-  */
+  // Código para mostrar el datetimepicker
   const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate;
+    const currentDate = selectedDate || date;
     setShow(false);
     setDate(currentDate);
-    /*
-    Codigo para convertir la fecha al formato año-mes-dia */
-
+    // Código para convertir la fecha al formato año-mes-día
     const year = currentDate.getFullYear();
     const month = String(currentDate.getMonth() + 1).padStart(2, '0');
     const day = String(currentDate.getDate()).padStart(2, '0');
-
     const fechaNueva = `${year}-${month}-${day}`;
-    setFechaNacimiento(fechaNueva)
+    setFechaNacimiento(fechaNueva);
   };
 
   const showMode = (currentMode) => {
@@ -73,37 +66,40 @@ export default function getUser({ navigation }) {
     navigation.navigate('Cambio');
   };
 
-    // Función para obtener los datos del usuario
-    const getUser = async () => {
-      try {
-        // Enviar una solicitud HTTP al servidor para obtener los datos del usuario
-        const response = await fetch(`${ip}/Sport_Development_3/api/services/public/cliente.php?action=getUser`, {
-          method: 'GET'
-        });
-        const data = await response.json();
-        if (data.status) {
-          // Si la solicitud es exitosa, actualizar el estado con el nombre del usuario
-          setNombre(data.name.nombre_cliente);
-          setApellido(data.name.apellido_cliente);
-          setEmail(data.name.correo_cliente);
-          setDui(data.name.dui_cliente);
-          setTelefono(data.name.telefono_cliente);
-          setDireccion(data.name.direccion_cliente);
-          setFechaNacimiento(data.name.nacimiento_cliente);
-        } else {
-          // Si hay un error, mostrar una alerta
-          Alert.alert('Error', data.error);
-        }
-      } catch (error) {
-        // Si hay un error de red, mostrar una alerta
-        Alert.alert('Error', 'Ocurrió un error al cerrar la sesión');
+  // Función para obtener los datos del usuario
+  const getUser = async () => {
+    try {
+      // Enviar una solicitud HTTP al servidor para obtener los datos del usuario
+      const response = await fetch(`${ip}/Sport_Development_3/api/services/public/cliente.php?action=getUser`, {
+        method: 'GET'
+      });
+      const data = await response.json();
+      if (data.status) {
+        // Si la solicitud es exitosa, actualizar el estado con el nombre del usuario
+        setNombre(data.name.nombre_cliente);
+        setApellido(data.name.apellido_cliente);
+        setEmail(data.name.correo_cliente);
+        setDui(data.name.dui_cliente);
+        setTelefono(data.name.telefono_cliente);
+        setDireccion(data.name.direccion_cliente);
+        setFechaNacimiento(data.name.nacimiento_cliente);
+        // Actualizar el estado date con la fecha de nacimiento del usuario
+        const [year, month, day] = data.name.nacimiento_cliente.split('-');
+        setDate(new Date(year, month - 1, day));
+      } else {
+        // Si hay un error, mostrar una alerta
+        Alert.alert('Error', data.error);
       }
-    };
-  
-    // Uso del React Hook useEffect para cargar los datos del usuario al montar el componente
-    useEffect(() => {
-      getUser();
-    }, []);
+    } catch (error) {
+      // Si hay un error de red, mostrar una alerta
+      Alert.alert('Error', 'Ocurrió un error al cerrar la sesión');
+    }
+  };
+
+  // Uso del React Hook useEffect para cargar los datos del usuario al montar el componente
+  useEffect(() => {
+    getUser();
+  }, []);
 
   // Función para crear un nuevo usuario
   const handleEdit = async () => {
@@ -151,11 +147,8 @@ export default function getUser({ navigation }) {
     }
   };
 
-
-
   return (
     <View style={styles.container}>
-
       <ScrollView contentContainerStyle={styles.scrollViewStyle}>
         <TouchableOpacity style={styles.ButtonVolver} onPress={volverInicio}>
           <AntDesign name="arrowleft" size={20} color="white" />
@@ -164,29 +157,28 @@ export default function getUser({ navigation }) {
         <Image source={require('../img/user.png')} style={styles.image} />
         <Buttons textoBoton='Cambiar foto de perfil' accionBoton={handleLogout} />
 
-
         <Input
           placeHolder='Nombre Cliente'
           setValor={nombre}
-          setTextChange={nombre}
+          setTextChange={setNombre}
         />
         <Input
           placeHolder='Apellido Cliente'
           setValor={apellido}
-          setTextChange={apellido}
+          setTextChange={setApellido}
         />
         <InputEmail
           placeHolder='Email Cliente'
           setValor={email}
-          setTextChange={email} />
+          setTextChange={setEmail} />
         <InputMultiline
           placeHolder='Dirección Cliente'
           setValor={direccion}
           valor={direccion}
-          setTextChange={direccion} />
+          setTextChange={setDireccion} />
         <MaskedInputDui
           dui={dui}
-          setDui={dui} />
+          setDui={setDui} />
         <View style={styles.contenedorFecha}>
           <TouchableOpacity onPress={showDatepicker}><Text style={styles.fechaSeleccionar}>Seleccionar Fecha de Nacimiento:  <Text style={styles.fecha}> {fechaNacimiento}</Text></Text></TouchableOpacity>
           {show && (
@@ -204,8 +196,7 @@ export default function getUser({ navigation }) {
 
         <MaskedInputTelefono
           telefono={telefono}
-          setTelefono={telefono} />
-
+          setTelefono={setTelefono} />
 
         <Buttons
           textoBoton='Editar perfil'
@@ -216,11 +207,8 @@ export default function getUser({ navigation }) {
           textoBoton='Cambiar contraseña'
           accionBoton={Cambio}
         />
-
       </ScrollView>
-
     </View>
-
   );
 }
 
@@ -256,23 +244,26 @@ const styles = StyleSheet.create({
     color: '#FFF', fontWeight: '500',
     fontSize: 25
   },
-
-  fecha: {
-    fontWeight: '500',
-    color: '#16537E'
-  },
-  fechaSeleccionar: {
-    fontWeight: '500',
-    color: '#16537E',
-  },
+  
   contenedorFecha: {
     backgroundColor: '#FFF',
-    color: "#16537E", fontWeight: '500',
+    flexDirection: 'row',
+    alignItems: 'center',
     width: 350,
     height: 45,
     borderRadius: 5,
     padding: 10,
     marginVertical: 10
+  },
+  fechaSeleccionar: {
+    fontSize: 13,
+    color: '#16537E',
+    fontWeight: '500'
+  },
+  fecha: {
+    fontSize: 13,
+    color: '#16537E',
+    fontWeight: '500'
   },
   image: {
     width: 75,
