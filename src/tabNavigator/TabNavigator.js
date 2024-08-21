aa69148dff8ab15e6837dd105fcea79093d0d5fb
AleGonzalez002 <denisalejandro2006@gmail.com>
2024-08-21 14:48:29 -0600

    Cambio en el tabnavigator

--- src/tabNavigator/TabNavigator.js
+++ src/tabNavigator/TabNavigator.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { Platform } from "react-native";
+import { Platform, View } from "react-native";
 
 // Importa tus componentes de pantalla aquí
 import Productos from "../screens/Productos";
@@ -20,9 +20,14 @@ const TabNavigator = () => {
         tabBarInactiveTintColor: "#FFF", // Color de los íconos inactivos
         tabBarStyle: {
           backgroundColor: "#16537E",
-          height: Platform.OS === "ios" ? 80 : 60, // Estilo de la barra de pestañas, altura diferente para iOS y Android
+          height: Platform.OS === "ios" ? 70 : 70, // Estilo de la barra de pestañas, altura diferente para iOS y Android
           borderTopWidth: 0,
-        }, // Estilo de la barra de pestañas
+        },
+        tabBarLabelStyle: {
+          fontSize: 12, 
+          fontWeight: "500",
+          paddingBottom: 5,
+        },
         tabBarIcon: ({ focused, color, size }) => {
           // Función que define el ícono de la pestaña
           let iconName;
@@ -35,7 +40,20 @@ const TabNavigator = () => {
           } else if (route.name === "Historial") {
             iconName = focused ? "time" : "time-outline";
           }
-          return <Ionicons name={iconName} color={color} size={size} />;
+          return (
+            <View
+              style={{
+                width: 50, 
+                height: 40, 
+                borderRadius: 15, 
+                backgroundColor: focused ? "#22699e" : "#16537E", 
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Ionicons name={iconName} color={color} size={size} />
+            </View>
+          );
         },
       })}
     >
